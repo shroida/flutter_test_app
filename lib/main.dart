78018327a4599c53a06492c7ae5e354d11c03029
shroida <shroida012@gmail.com>
2026-05-17 10:28:03 +0300

    main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'features/counter/data/repositories/counter_repository_impl.dart';
+import 'features/counter/domain/usecases/decrement_usecase.dart';
+import 'features/counter/domain/usecases/increment_usecase.dart';
+import 'features/counter/presentation/cubit/counter_cubit.dart';
+import 'features/counter/presentation/pages/counter_page.dart';
+
 void main() {
-  runApp(const MyApp());
+
+  final repository = CounterRepositoryImpl();
+
+  runApp(MyApp(repository: repository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final CounterRepositoryImpl repository;
+
+  const MyApp({
+    super.key,
+    required this.repository,
+  });
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Test App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+      home: BlocProvider(
+        create: (_) => CounterCubit(
+          incrementUseCase: IncrementUseCase(repository),
+          decrementUseCase: DecrementUseCase(repository),
+        ),
+        child: const CounterPage(),
       ),
-      home: const CounterPage(),
     );
   }
 }
