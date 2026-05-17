cda528b987e4d033a59ac3659bb5f7c6e0c26ca4
shroida <shroida012@gmail.com>
2026-05-17 17:37:11 +0300

    apply TDD

--- lib/features/counter/data/repositories/counter_repository_impl.dart
+++ lib/features/counter/data/repositories/counter_repository_impl.dart
@@ -4,14 +4,8 @@ import 'package:flutter_test_app/features/counter/domain/repositories/counter_re
 class CounterRepositoryImpl implements CounterRepository {
 
   @override
-  int increment(int value) {
-     
-    return value+1;
-  }
+  int increment(int value) => value + 1;
 
   @override
-  int decrement(int value) {
-     
-    return value-1;
-  }
+  int decrement(int value) => value - 1;
 }
